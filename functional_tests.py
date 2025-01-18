119c12dfc755521b2e040638760c5be57268a1bc
Brandon Conder <75953518+BrandonConder@users.noreply.github.com>
2025-01-18 12:02:12 -0700

    Refactor complete

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_todo_list(self):
         # Let's check on our to-do list
         self.browser.get('http://localhost:8000')
@@ -28,13 +33,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(
-            '1: Buy peacock feathers',
-            [row.text for row in rows],
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # Text box must allow another entry
 
@@ -45,17 +44,10 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # Page update, with both items
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(
-            '1: Buy peacock feathers',
-            [row.text for row in rows],
-        )
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows],
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
+        # Test complete
 
 if __name__ == '__main__':
     unittest.main()
